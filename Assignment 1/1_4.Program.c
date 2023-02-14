47652e73ab4d59e2bd71e7cd6248f439f0c4ffae
Abhishek Desai <93270535+Desaiabhishek@users.noreply.github.com>
2023-02-14 19:55:01 +0530

    first Assignment

---
+++ Assignment 1/1_4.Program.c
@@ -0,0 +1,41 @@
+//Accept one Number and check whether is divisible by 5 or not
+
+#include<stdio.h>
+
+typedef int BOOL;
+#define TRUE 1
+#define FALSE 0
+
+BOOL Check(int iNo)
+{
+	if( iNo % 5 == 0)
+	{
+		return TRUE;
+	}
+	else
+	{
+		return FALSE;
+	}
+}
+
+int main()
+{
+	int iValue = 0;
+	BOOL bRet = FALSE;
+	
+	printf("Enter Number : ");
+	scanf("%d",&iValue);
+	
+	bRet = Check(iValue);
+	
+	if(bRet == TRUE)
+	{
+		printf("\n Divisible by 5.");
+	}
+	else
+	{
+		printf("\n Not Divisible by 5.");
+	}
+	 
+	 return 0;
+}

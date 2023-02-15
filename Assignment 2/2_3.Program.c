e99eb9631d3ed6f40f63f470c8bc1ef670649fe5
Abhishek Desai <93270535+Desaiabhishek@users.noreply.github.com>
2023-02-15 07:59:18 +0530

    Second Assignment

---
+++ Assignment 2/2_3.Program.c
@@ -0,0 +1,27 @@
+//Accept two numbers from user and display first number in second number of times
+
+#include<stdio.h>
+
+void Display( int iNo)
+{
+	if(	iNo < 10)
+	{
+		printf("Hello");
+	}
+	else
+	{
+		printf("Demo");
+	}
+}
+
+int main()
+{
+	int iValue = 0;
+	
+	printf("Enter Number : ");
+	scanf("%d",&iValue);
+	
+	Display(iValue);
+	
+	return 0;
+}	

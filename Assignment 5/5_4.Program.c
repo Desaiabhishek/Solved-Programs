ae3fe38a5e38d20cd227b341df21c9819fa2741e
Abhishek Desai <93270535+Desaiabhishek@users.noreply.github.com>
2023-03-05 19:45:28 +0530

    accept number and return summation of all ist non factors

---
+++ Assignment 5/5_4.Program.c
@@ -0,0 +1,33 @@
+//write a program which accept number from user and return summation of  all its non factors
+
+#include<stdio.h>
+
+int SumNonFact(int iNo)
+{
+	int iCnt = 0;
+	int iSum = 0;
+	
+	for(iCnt = 1; iCnt < iNo; iCnt++)
+	{
+		if(iNo % iCnt != 0)
+		{
+			iSum = iSum + iCnt;
+		}
+	}
+	return iSum;
+}
+
+int main()
+{
+	int iValue = 0;
+	int iRet = 0;
+	
+	printf("Enter Number : ");
+	scanf("%d",&iValue);
+	
+	iRet = SumNonFact(iValue);
+	
+	printf("Summation of non factors are : %d",iRet);
+	
+	return 0;
+}

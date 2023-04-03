37be93e3bad0f1997c2dc5b88c1385e66d57ddc7
Abhishek Desai <93270535+Desaiabhishek@users.noreply.github.com>
2023-04-03 20:03:54 +0530

    accept range from user and display all numbers in between that range

---
+++ Assignment 11/11_1.Program.c
@@ -0,0 +1,35 @@
+//Write a program which accept range from user and display all numbers in between that range
+
+#include<stdio.h>
+
+void RangeDisplay(int iStart,int iEnd)
+{
+	int iCnt = 0;
+	
+	if(iStart <= iEnd)
+	{
+		for(iCnt = iStart; iCnt <= iEnd;iCnt++)
+		{
+			printf("%d ",iCnt);
+		}
+	}
+	else
+	{
+		printf("Invalid Range");
+	}
+}
+
+int main()
+{
+	int iValue1 = 0, iValue2 = 0;
+	
+	printf("Enter Starting Point : ");
+	scanf("%d",&iValue1);
+	
+	printf("Enter ending point : ");
+	scanf("%d",&iValue2);
+	
+	RangeDisplay(iValue1,iValue2);
+	
+	return 0;
+}

c8344b4793a5da43a2e9c7a5e04e87d46a5c23fa
Abhishek Desai <93270535+Desaiabhishek@users.noreply.github.com>
2023-04-06 09:04:11 +0530

    accept range from user and return addition of all even numbers in between that range

---
+++ Assignment 11/11_4.Program.c
@@ -0,0 +1,42 @@
+//Write a program which accept range from user and return addition of  all Even numbers in between that range.(range should contains positive numbers only)
+
+#include<stdio.h>
+
+int RangeSumEven(int iStart,int iEnd)
+{
+	int iCnt = 0,iSum = 0;
+	
+	if(iStart <= iEnd && iStart > 0)
+	{
+		for(iCnt = iStart; iCnt <= iEnd;iCnt++)
+		{
+			if(iCnt % 2 == 0)
+			{
+				iSum = iSum + iCnt;
+			}
+		}
+	}
+	else
+	{
+		printf("Invalid Range");
+	}
+	
+	return iSum;
+}
+
+int main()
+{
+	int iValue1 = 0, iValue2 = 0, iRet = 0;
+	
+	printf("Enter Starting Point : ");
+	scanf("%d",&iValue1);
+	
+	printf("Enter ending point : ");
+	scanf("%d",&iValue2);
+	
+	iRet = RangeSumEven(iValue1,iValue2);
+	
+	printf("\n Addition is %d",iRet);
+	
+	return 0;
+}
